11ee1472cc99890da11663e36e29b08baf8d04f4
Stanislav Kiselevskii <st.kiselevskii@proton.me>
2026-01-01 19:30:29 +0100

    CD-09: Add a simplest console wrapper for the future use

---
+++ Src/CMP.Lib/Rpt/ConsoleReportService.cs
@@ -0,0 +1,13 @@
+﻿namespace CMP.Lib.Rpt;
+
+public sealed class ConsoleReportService : IReportService
+{
+    public void Info(string message)
+        => Console.WriteLine(message);
+
+    public void Warning(string message)
+        => Console.WriteLine($"[WARN] {message}");
+
+    public void Error(string message)
+        => Console.Error.WriteLine($"[ERR ] {message}");
+}
---
+++ Src/CMP.Lib/Rpt/IReportService.cs
@@ -0,0 +1,8 @@
+﻿namespace CMP.Lib.Rpt;
+
+public interface IReportService
+{
+    void Info(string message);
+    void Warning(string message);
+    void Error(string message);
+}
